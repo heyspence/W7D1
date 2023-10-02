a8b151ed5345074f9da838a7697a31e5a692174d
“Spencer <Spencer@bookservo.com>
2023-10-02 15:02:07 -0400

    user model

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,47 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    before_validation :ensure_session_token 
+    attr_reader :password
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        user&.is_password?(password) ? user : nil
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+    def ensure_session_token
+        @session_token ||= generate_unique_session_token
+    end
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64
+            return sesson_token unless User.exists?(session_token: session_token)
+        end
+    end
+end
